d9a1887b1cf594785f68394f20e4af1ed8cc657a
Anatoly_Kameko <kameko2008@yandex.ru>
2020-02-17 18:46:08 +0300

    Не понимаю как работает гит, опять добавляю калькулятор

--- app/src/main/java/com/example/gb_android_client/Calculator/CalculatorFragment.java
+++ app/src/main/java/com/example/gb_android_client/Calculator/CalculatorFragment.java
@@ -8,14 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.gb_android_client.R;
+import com.example.gb_android_client.myMVP.MyPresenter;
 import com.example.gb_android_client.myMVP.MyView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CalculatorFragment extends Fragment implements MyView {
+    CalculatorPresenter presenter;
+    TextView textViewResult;
 
 
     public CalculatorFragment() {
@@ -27,7 +33,42 @@ public class CalculatorFragment extends Fragment implements MyView {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false);
+        View root = inflater.inflate(R.layout.fragment_calculator, container, false);
+        Button calcButton = root.findViewById(R.id.button_calc);
+        final EditText editHeight = root.findViewById(R.id.calc_edit_text_height);
+        final EditText editLenght = root.findViewById(R.id.calc_edit_text_lenght);
+        textViewResult = root.findViewById(R.id.calc_text_view_result);
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editHeight.getText().toString().equals("") && !editLenght.getText().toString().equals("")) {
+                    presenter.calculatorStart(Integer.parseInt(editHeight.getText().toString()),
+                            Integer.parseInt(editLenght.getText().toString()));
+                }
+
+            }
+        });
+
+        presenter = new CalculatorPresenter();
+        presenter.attachView(this);
+        return root;
     }
 
+    public void showResult(int stairsFrameCount, int passFrameCount, int diagonalConnectionCount,
+                           int horizontalConnectionCount, int crossbarCount, int deckCount,
+                           int supportsCount, double costPerDay) {
+        textViewResult.setText(String.format("Рама с лестницей = %s, Рама проходная = %s, Связь диагональная = %s, " +
+                        "Связь горизонтальная = %s, Ригель настила = %s, Настил деревянный = %s, Опоры (пятки) = %s, стоимость в день %s",
+                stairsFrameCount, passFrameCount, diagonalConnectionCount,
+                horizontalConnectionCount, crossbarCount, deckCount,
+                supportsCount, costPerDay));
+        textViewResult.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        presenter.detachView();
+    }
 }
--- app/src/main/java/com/example/gb_android_client/Calculator/CalculatorPresenter.java
+++ app/src/main/java/com/example/gb_android_client/Calculator/CalculatorPresenter.java
@@ -4,7 +4,29 @@ import com.example.gb_android_client.myMVP.MyPresenter;
 import com.example.gb_android_client.myMVP.MyView;
 
 public class CalculatorPresenter implements MyPresenter {
-    MyView view;
+
+    CalculatorFragment view;
+
+    double squareMeterCost = 0;
+
+    /*Переменные:
+    Высота - H;
+    Длина - L;
+    Количество ярусов - F = H/2;
+    Количество секция - S = L/3;
+    Количество подъемов - R (1 подъем на 30 м длины);
+    Количество ярусов с настилами - D = F - 1;*/
+
+
+    private int stairsFrameCount;   //Рама с лестницей = (F - 1) * R;
+    private int passFrameCount;     //Рама проходная = F * ((S + 1) - D) + D;
+    private int diagonalConnectionCount;    // Связь диагональная = (H/3 * L/2)/2;
+    private int horizontalConnectionCount;  //Связь горизонтальная = Связь диагональная * 3;
+    private int crossbarCount;  //Ригель настила = (D * S) * 2;
+    private int deckCount;  //Настил деревянный = (S * D) * 3;
+    private int supportsCount;  //Опоры (пятки) = ((L/3) + 1) * 2;
+    private double costPerDay;  //стоимость в день
+
 
     @Override
     public void detachView() {
@@ -13,7 +35,37 @@ public class CalculatorPresenter implements MyPresenter {
 
     @Override
     public void attachView(MyView view) {
-        this.view = view;
+        this.view = (CalculatorFragment)view;
+    }
+
+
+
+    public void calculatorStart(int height, int length){
+        calculate(height,length);
+        view.showResult(stairsFrameCount, passFrameCount, diagonalConnectionCount,
+        horizontalConnectionCount, crossbarCount, deckCount,
+        supportsCount, costPerDay);
+    }
+
+    private void calculate (int height, int length) {
+
+        if (height%2 == 1) height++;
+        if (length%3 == 2) length++;
+        if (length%3 == 1) length--;
+        int levelCount = height/2;
+        int sectionCount = length/3;
+        int liftCount = 1 + (length/30);
+        int deckLevelCount = 1;
+        if (levelCount - 1 > 0) deckLevelCount = levelCount - 1;
+
+        stairsFrameCount = (levelCount-1) * liftCount;
+        passFrameCount = levelCount * ((sectionCount + 1) - deckLevelCount) + deckLevelCount;
+        diagonalConnectionCount = ((height/3) * (length/2))/2;
+        horizontalConnectionCount = diagonalConnectionCount * 3;
+        crossbarCount = deckLevelCount * sectionCount * 2;
+        deckCount = sectionCount * deckLevelCount * 3;
+        supportsCount = (sectionCount + 1) * 2;
+        costPerDay = height * length * squareMeterCost;
     }
 
 
